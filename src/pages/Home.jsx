75ecc7dc63acb17a97635e9aae9c7ac3a613dccc
Noic Noic <galanisd@csd.auth.gr>
2022-12-16 20:01:58 +0200

    dummy posts from api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import sea from "../images/sea.jpg";
 import mountain from "../images/mountain.jpg";
 import football from "../images/football.jpg";
 import ImageSlider from "../components/ImageSlider.jsx";
 import Abc from "../components/Abc";
 import SportPosts from "../components/SportPosts";
+import axios from "axios";
 
 const slides = [
   {
@@ -22,13 +25,45 @@ const slides = [
 ];
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("/posts/");
+        setPosts(res.data);
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="bg-gray-50 pt-2 shadow-md">
       <div className="container mx-auto pl-3">
         <span className="text-gray-800 text-xl border-l-2 pl-2 border-red-600 md:text-2xl font-medium">
-          Latest News
+          Latest News {posts[0]?.id}
         </span>
       </div>
+
+      {/* <div className="posts">
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={post.img} />
+            </div>
+            <div className="content">
+              <div className="link">
+                <h1>{post.title}</h1>
+              </div>
+
+              <button>Read More</button>
+            </div>
+          </div>
+        ))}
+      </div> */}
 
       <ImageSlider slides={slides} />
 
